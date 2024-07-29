efd7d9b86cf1a4c8da5bd3bd179b8b2bce7cfe9f
RomChicCampNum <romain.chica@le-campus-numerique.fr>
2024-07-29 15:48:27 +0200

    third commit adding routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return 'Homepage';
+});
+
+Route::get('/product', function () {
+    return 'Liste des produits';
+});
+
+Route::get('/product/{id}', function () {
+    return 'Fiche du produit {id}';
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
+});
